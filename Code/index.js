c302ce1b46d93e585c500b3745da03b3ae354277
Levente Tarsoly <tarsoly.levente03@gmail.com>
2024-03-22 20:32:10 +0100

    prettier

--- Code/index.js
+++ Code/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get
+app.get;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.listen(3000, () => { console.log("Listening @ 3000")})
+app.listen(3000, () => {
+  console.log("Listening @ 3000");
+});
